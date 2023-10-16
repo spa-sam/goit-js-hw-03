cc15befbdf335c62e0d7f3cebcf84303784f99ce
Pablo <st.spasam@gmail.com>
2023-10-16 14:05:43 +0300

    work

---
+++ task-2.js
@@ -0,0 +1,12 @@
+function makeArray(firstArray, secondArray, maxLength) {
+    newArray = firstArray.concat(secondArray);
+    newArray.length > maxLength ? newArray.length = maxLength : newArray.length;
+    return newArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
